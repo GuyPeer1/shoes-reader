28d0ef9e07a919f78692352e6a8f418fc2b7fe40
Guy Peer <peer.g2@gmail.com>
2023-05-01 14:33:08 +0300

    rfid backend service now gets the correct COM path automatically

--- backend/services/rfid.service.js
+++ backend/services/rfid.service.js
@@ -1,34 +1,46 @@
 const { SerialPort } = require('serialport')
 const socketService = require('./socket.service.js')
 
-//linux:
-//path 1:         path: '/dev/ttyUSB0',
+//this is the manufatrer name of our rfid reader
+const MANUFACTURER_NAME = 'Silicon Labs'
 
-function readFirstRfid() {
-    const firstPort = new SerialPort({
-        path: '/COM6',
+//the following function will get the correct path of the rfid's will open its serial port using readRfid func
+getPortPath(MANUFACTURER_NAME)
+
+async function getPortPath(manufacturerName) {
+    const ports = await SerialPort.list()
+    const filteredPorts = ports.filter(port => port.manufacturer === manufacturerName)
+    if (filteredPorts.length === 0) {
+        throw new Error(`No ports found with manufacturer name ${manufacturerName}`)
+    }
+    const PORT_PATH = filteredPorts[0].path
+    readRfid(PORT_PATH, 'rfid')
+}
+
+function readRfid(path, eventType) {
+    const port = new SerialPort({
+        path,
         baudRate: 9600
-    })
+    });
     let buffer = Buffer.alloc(0)
 
-    firstPort.on('data', data => {
-        buffer = Buffer.concat([buffer, data]);
+    port.on('data', data => {
+        buffer = Buffer.concat([buffer, data])
 
         if (buffer.length >= 16) {
             const tag_id = buffer.slice(3, 15).toString('hex')
-            console.log(tag_id);
+            console.log(tag_id)
             buffer = Buffer.alloc(0)
+            console.log(eventType, tag_id)
 
             // Emit the RFID code via socket
-            socketService.emitTo({ type: 'rfid', data: tag_id })
-            console.log('first',tag_id)
-            firstPort.close(() => {
-                console.log('First Port closed')
-                readFirstRfid()
+            socketService.emitTo({ type: eventType, data: tag_id })
+
+            port.close(() => {
+                console.log(`Port ${path} closed`)
+                readRfid(path, eventType)
             })
         }
     })
 }
-
-readFirstRfid()
 
