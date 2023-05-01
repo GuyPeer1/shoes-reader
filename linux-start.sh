e484e0af47c82e20c43bb69597b1c9fd7a1db394
Guy Peer <88733855+GuyPeer1@users.noreply.github.com>
2023-05-01 19:39:52 +0300

    Update linux-start.sh

--- linux-start.sh
+++ linux-start.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+echo "Starting servers..."
+
 # Change to backend directory
 cd /backend
+echo "Current working directory: $(pwd)"
 
 # Start backend server
 npm start &
+echo "Backend server started."
 
 # Wait for backend server to start
 sleep 10
 
 # Change to frontend directory
 cd /frontend
+echo "Current working directory: $(pwd)"
 
 # Start frontend server
 npm start
+echo "Frontend server started."
+
+echo "Servers started."
